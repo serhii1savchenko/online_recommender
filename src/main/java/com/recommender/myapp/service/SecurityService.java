baa2837fbefeb493134237fe4040d44be4599614
sergey1savchenko <w1nner1715@gmail.com>
2017-03-24 09:03:01 +0200

    let's start over

--- src/main/java/com/recommender/myapp/service/SecurityService.java
+++
@@ -1,8 +0,0 @@
-package com.recommender.myapp.service;
-
-public interface SecurityService {
-	
-	public String findLoggedInUsername();
-	public void autoLogin(String username, String password);
-	
-}
